a09fc171ab508f0065f6acf90c6c6ba979115f35
RutgiFree <roger7a7@gmail.com>
2024-03-20 17:22:48 +0100

    first mesh corve

--- RogerRodriguezMendez_tfg_videojocs/Assets/MeshGenerator.cs
+++ RogerRodriguezMendez_tfg_videojocs/Assets/MeshGenerator.cs
@@ -5,11 +5,47 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
+    public class MeshNode {
+        int lenght;
+        int sectionLenght;
+
+        MeshGenerator meshG;
+
+        MeshNode diferentSectionNode;
+
+
+        Mesh newMesh;
+
+        public MeshNode(int _lenght, MeshGenerator _meshG)
+        {
+            lenght = _lenght;
+            sectionLenght = 1;
+            meshG = _meshG;
+        }
+        
+        public Mesh goDiferentSameSection(Mesh oldMesh) {
+            //primer generem la nova mesh en referencia al vell
+
+            if (oldMesh == null) newMesh = meshG.myGenerarMeshNode(sectionLenght);
+            else newMesh = meshG.myUpdateMeshNode(oldMesh, sectionLenght);
+
+            if (lenght - (sectionLenght)<= 0) return newMesh;//hem arribat al final
+
+            diferentSectionNode = new MeshNode(lenght - 1, meshG);
+
+            return diferentSectionNode.goDiferentSameSection(newMesh);
+        }
+
+        
+
+    }
 
     [SerializeField] bool generate;
 
     [SerializeField] int WorldY = 1;
     Mesh mesh;
+    GameObject spanwer;
+    
 
     /*Un quadrat esta format per 2 triangles
      *    O---O
@@ -32,20 +68,24 @@ public class MeshGenerator : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        myGenerarMesh(WorldY);
-
+        spanwer = new GameObject("spanwer");
+        spanwer.transform.parent = transform;
+        spanwer.transform.position = transform.localPosition;
+        //myGenerarMesh(WorldY);
     }
-
-    void myGenerarMesh(int numbY)
+    public Mesh myGenerarMeshNode(int numbY)
     {
-        int[] myTriangles = new int[ numbY * 6];
+        int[] myTriangles = new int[numbY * 6];
         Vector3[] myVertexs = new Vector3[(2) * (numbY + 1)];
 
         int i = 0;
         for (int y = 0; y <= numbY; y++)
         {
-                myVertexs[i++] = new Vector3(-0.5f, y, 0);
-                myVertexs[i++] = new Vector3(0.5f, y, 0);
+            //es quade en ordre ascendent cap a Y, per tant,
+            //priumer guardem el V3 dels V 0 i 2, despres els 1 i 3, etc
+            myVertexs[i++] = new Vector3(spanwer.transform.localPosition.x - 0.5f, spanwer.transform.localPosition.y + y, 0);
+            myVertexs[i++] = new Vector3(spanwer.transform.localPosition.x + 0.5f, spanwer.transform.localPosition.y + y, 0);
+            spanwer.transform.position += Vector3.up * y;
         }
 
         int mesT = 0;
@@ -53,7 +93,108 @@ public class MeshGenerator : MonoBehaviour
 
         for (int y = 0; y < numbY; y++)
         {
+            //estem ficant l'index d'on es trove el vector que volem
+            myTriangles[mesT + 0] = vIndex + 0;
+            myTriangles[mesT + 1] = vIndex + 2;
+            myTriangles[mesT + 2] = vIndex + 1;
 
+            myTriangles[mesT + 3] = vIndex + 1;
+            myTriangles[mesT + 4] = vIndex + 2;
+            myTriangles[mesT + 5] = vIndex + 3;
+
+            mesT += 6;
+            vIndex += 2;
+        }
+
+        Mesh nodeMesh = new Mesh();
+        nodeMesh.vertices = myVertexs;
+        nodeMesh.triangles = myTriangles;
+
+        nodeMesh.RecalculateNormals();
+        return nodeMesh;
+    }
+
+    public Mesh myUpdateMeshNode(Mesh oldMesh, int numbY)
+    {
+        int[] newTriangles = new int[numbY * 6];
+        Vector3[] newVertexs = new Vector3[2 * numbY];
+
+        float baseY = oldMesh.vertices[oldMesh.vertices.Length - 1].y;
+        int i = 0;
+
+        float deviationDegrees = 50;
+
+
+        for (float y = 1; y <= numbY; y++)
+        {
+            var left = new GameObject("left");
+            left.transform.parent = spanwer.transform;
+            left.transform.localPosition =  (Vector3.left * 0.5f);
+
+            var center = new GameObject("center");
+            center.transform.parent = spanwer.transform;
+            center.transform.localPosition = (Vector3.up);
+
+            var right = new GameObject("right");
+            right.transform.parent = spanwer.transform;
+            right.transform.localPosition = (Vector3.right * 0.5f);
+
+            spanwer.transform.rotation = Quaternion.AngleAxis(deviationDegrees, Vector3.forward);
+            center.transform.parent = transform;
+            spanwer.transform.localPosition = center.transform.localPosition;
+
+            left.transform.parent = transform;
+            right.transform.parent = transform;
+
+            newVertexs[i++] = left.transform.localPosition;
+            newVertexs[i++] = right.transform.localPosition;
+
+
+        }
+
+        int mesT = 0;
+        int vIndex = (oldMesh.triangles.Length / 6) * 2;
+
+        for (int y = 0; y < numbY; y++)
+        {
+            newTriangles[mesT + 0] = vIndex + 0;
+            newTriangles[mesT + 1] = vIndex + 2;
+            newTriangles[mesT + 2] = vIndex + 1;
+
+            newTriangles[mesT + 3] = vIndex + 1;
+            newTriangles[mesT + 4] = vIndex + 2;
+            newTriangles[mesT + 5] = vIndex + 3;
+            mesT += 6;
+            vIndex += 2;
+        }
+
+        oldMesh.vertices = oldMesh.vertices.Concat(newVertexs).ToArray();
+        oldMesh.triangles = oldMesh.triangles.Concat(newTriangles).ToArray();
+        
+        oldMesh.RecalculateNormals();
+        return oldMesh;
+    }
+    /*
+    public void myGenerarMesh(int numbY)
+    {
+        int[] myTriangles = new int[ numbY * 6];
+        Vector3[] myVertexs = new Vector3[(2) * (numbY + 1)];
+
+        int i = 0;
+        for (int y = 0; y <= numbY; y++)
+        {
+            //es quade en ordre ascendent cap a Y, per tant,
+            //priumer guardem el V3 dels V 0 i 2, despres els 1 i 3, etc
+            myVertexs[i++] = new Vector3(-0.5f, y, 0);
+            myVertexs[i++] = new Vector3(0.5f, y, 0);
+        }
+
+        int mesT = 0;
+        int vIndex = 0;
+
+        for (int y = 0; y < numbY; y++)
+        {
+            //estem ficant l'index d'on es trove el vector que volem
             myTriangles[mesT + 0] = vIndex + 0;
             myTriangles[mesT + 1] = vIndex + 2;
             myTriangles[mesT + 2] = vIndex + 1;
@@ -73,7 +214,7 @@ public class MeshGenerator : MonoBehaviour
         mesh.RecalculateNormals();
     }
 
-    void myUpdateMesh(Vector3[] actualV, int[] actualT, int numbY)
+    public void myUpdateMesh(Vector3[] actualV, int[] actualT, int numbY)
     {
 
         int[] newTriangles = new int[ numbY * 6];
@@ -108,12 +249,22 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.RecalculateNormals();
     }
+    */
     void Update()
     {
         if (generate)
         {
             generate = !generate;
-            myUpdateMesh(mesh.vertices, mesh.triangles, WorldY);
+            //myUpdateMesh(mesh.vertices, mesh.triangles, WorldY);
+
+            spanwer.transform.position = transform.localPosition;
+            MeshNode node = new MeshNode(WorldY, this);
+            Mesh newMesh = node.goDiferentSameSection(null);
+
+            mesh.Clear();
+            mesh.vertices = newMesh.vertices;
+            mesh.triangles = newMesh.triangles;
+
         }
     }
 
