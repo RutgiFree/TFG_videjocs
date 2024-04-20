fe8f7be73d5dd5bda52d420bf69a5e3ee16d8855
RutgiFree <roger7a7@gmail.com>
2024-04-20 12:24:59 +0200

    2 meshes unified

--- RogerRodriguezMendez_tfg_videojocs/Assets/SCRIPTS/VegetableScripts/VegetableProxyMesh.cs
+++ RogerRodriguezMendez_tfg_videojocs/Assets/SCRIPTS/VegetableScripts/VegetableProxyMesh.cs
@@ -2,24 +2,67 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class VegetableProxyMesh : MonoBehaviour
 {
+    public class MeshInfo
+    {
+        Vector3[] vertices;
+        int[] triangles;
+
+        public MeshInfo(): this(new Vector3[0], new int[0]) { }
+        public MeshInfo(Vector3[] _vertices, int[] _triangles)
+        {
+            vertices = _vertices;
+            triangles = _triangles;
+        }
+
+        public void setVertices(Vector3[] _vertices) { vertices = _vertices; }
+        public void setTriangles(int[] _triangles) { triangles = _triangles; }
+
+        public Vector3[] getVertices() { return vertices; }
+        public int[] getTriangles() { return triangles; }
+
+        public bool isEmpty() { return vertices.Length == 0 && triangles.Length == 0;  }
+
+    }
+    public class SectionsController
+    {
+        MeshInfo meshInfo;
+        string rules;
+        int sectionIndex;
+        bool sectionEnded;
+
+        public SectionsController(MeshInfo _meshInfo, int _sectionIndex, bool _sectionEnded)
+        {
+            meshInfo = _meshInfo;
+            rules = "";
+            sectionIndex = _sectionIndex;
+            sectionEnded = _sectionEnded;
+        }
+
+        public void EndSection(string _rules)
+        {
+            sectionEnded = true;
+            rules = _rules;
+        }
+        public void setRules(string _rules) { rules = _rules; }
+        public void setMeshInfo(MeshInfo _meshInfo) { meshInfo = _meshInfo; }
+
+        public MeshInfo getMeshInfo() { return meshInfo; }
+        public string getRules() { return rules; }
+        public int getIndex() { return sectionIndex; }
+        public bool getEnded() { return sectionEnded; }
+    }
+
     public class MeshNode
     {
-        public class SectionsController
-        {
-            public string rules;
-            public Mesh mesh;
-            public int sectionIndex;
-            public bool sectionEnded;
-        }
 
         float sectionDegree;
         Vector3 startPoint;
-        Mesh sectionMesh;
         SectionsController sectionsController;
 
         Spawner spawner;
@@ -34,25 +77,19 @@ public class VegetableProxyMesh : MonoBehaviour
             spawner = _spawner;
             startPoint = _startPoint;
 
-            sectionsController = new SectionsController();
-            sectionsController.mesh = new Mesh();
-            sectionsController.sectionIndex = sectionIndex;
-            sectionsController.sectionEnded = false;
-
-            sectionMesh = new Mesh();
-
+            sectionsController = new SectionsController(new MeshInfo(), sectionIndex, false);
         }
 
         public SectionsController startGeneration(string rules)
         {
-            sectionsController.rules = rules;
-            Mesh generalMesh = null;
+            sectionsController.setRules(rules);
+            MeshInfo generalMeshInfo = new MeshInfo();
 
             var myUnSeenRules = rules;
 
             foreach (char c in myUnSeenRules)
             {
-                if (!spawner.translateStandartRules(c, sectionMesh))
+                if (!spawner.translateStandartRules(c, sectionsController.getMeshInfo()))
                 {
                     if ((Rules.DNAnucleotides)c == Rules.DNAnucleotides.START_BRANCH)
                     {
@@ -62,60 +99,59 @@ public class VegetableProxyMesh : MonoBehaviour
                         sectionDegree = spawner.getRotation();
 
                         var auxiliar = goDifSection(difSectionRules);
-                        generalMesh = spawner.UnifyMeshes(sectionMesh, auxiliar.mesh);
+                        generalMeshInfo = spawner.UnifyTwoMeshes(sectionsController.getMeshInfo(), auxiliar.getMeshInfo());
 
                         spawner.setPositionAndDegree(startPoint, sectionDegree);
-                        string sameSectionRules = auxiliar.rules;
+                        string sameSectionRules = auxiliar.getRules();
 
                         //despres de la diferent hi ha mes coses?
                         if (sameSectionRules.Length == 0)
                         {
                             //no hi ha res mes a fer, marxem
-                            sectionsController.mesh = generalMesh;
-                            sectionsController.rules = "";
-                            sectionsController.sectionEnded = true;
+                            sectionsController.setMeshInfo(generalMeshInfo);
+                            sectionsController.EndSection("");
                             return sectionsController;
                         }
 
                         auxiliar = goSameSection(sameSectionRules);
-                        generalMesh = spawner.UnifyMeshes(generalMesh, auxiliar.mesh);
+                        generalMeshInfo = spawner.UnifyMeshes(generalMeshInfo, auxiliar.getMeshInfo());
 
-                        if (auxiliar.sectionEnded && auxiliar.sectionIndex == sectionsController.sectionIndex)
+                        if (auxiliar.getEnded() && auxiliar.getIndex() == sectionsController.getIndex())
                         {
                             //en la meva seccio he trobat un final, per tant, retornem el que tenim
-                            sectionsController.mesh = generalMesh;
-                            sectionsController.rules = auxiliar.rules;
-                            sectionsController.sectionEnded = true;
+                            sectionsController.setMeshInfo(generalMeshInfo);
+                            sectionsController.EndSection(auxiliar.getRules());
                             return sectionsController;
                         }
 
-                        myUnSeenRules = auxiliar.rules;
+                        myUnSeenRules = auxiliar.getRules();
                         break;
 
                     }
                     if ((Rules.DNAnucleotides)c == Rules.DNAnucleotides.END_BRANCH)
                     {
-                        sectionsController.mesh = sectionMesh;
-                        sectionsController.rules = myUnSeenRules.Split(']', 2)[1];
-                        sectionsController.sectionEnded = true;
+                        sectionsController.setMeshInfo(sectionsController.getMeshInfo());
+                        sectionsController.EndSection(myUnSeenRules.Split(']', 2)[1]);
                         return sectionsController;
                     }
                 }
             }
-            sectionsController.mesh = generalMesh ?? sectionMesh;
-            sectionsController.rules = "";
+            //sectionsController.setRules("");
+            if (generalMeshInfo.isEmpty()) sectionsController.setMeshInfo(sectionsController.getMeshInfo());
+            else sectionsController.setMeshInfo(generalMeshInfo);
+            sectionsController.EndSection("");
             spawner.setPositionAndDegree(startPoint, sectionDegree);
             return sectionsController;
         }
 
         SectionsController goDifSection(string rules)
         {
-            if (diferentSectionNode == null) diferentSectionNode = new MeshNode(spawner.getPositionCenter(), spawner, sectionsController.sectionIndex + 1);
+            if (diferentSectionNode == null) diferentSectionNode = new MeshNode(spawner.getPositionCenter(), spawner, sectionsController.getIndex() + 1);
             return diferentSectionNode.startGeneration(rules);
         }
         SectionsController goSameSection(string rules)
         {
-            if (sameSectionNode == null) sameSectionNode = new MeshNode(spawner.getPositionCenter(), spawner, sectionsController.sectionIndex);
+            if (sameSectionNode == null) sameSectionNode = new MeshNode(spawner.getPositionCenter(), spawner, sectionsController.getIndex());
             return sameSectionNode.startGeneration(rules);
         }
     }
@@ -124,12 +160,14 @@ public class VegetableProxyMesh : MonoBehaviour
     {
         GameObject parent, center, growCenter, left, right;
         MeshFilter meshFilterRecipient;
+        float initialDegree;
         public Spawner(GameObject _parent)
         {
             parent = _parent;
             center = new GameObject("center-spawner");
             center.transform.parent = _parent.transform;
             center.transform.position = _parent.transform.localPosition;
+            initialDegree = _parent.transform.eulerAngles.z;
             meshFilterRecipient = center.AddComponent<MeshFilter>();
 
             left = new GameObject("left-spawner");
@@ -153,65 +191,97 @@ public class VegetableProxyMesh : MonoBehaviour
             center.transform.localPosition = newPosition;
             center.transform.rotation = Quaternion.AngleAxis(newDegree, Vector3.forward);
         }
+        public void resetAllPositions()
+        {
+            center.transform.localPosition = Vector3.zero;
+            center.transform.rotation = Quaternion.AngleAxis(initialDegree, Vector3.forward);
+            left.transform.localPosition = (Vector3.left * 0.5f);
+            right.transform.localPosition = (Vector3.right * 0.5f);
+            growCenter.transform.localPosition = (Vector3.up);
+        }
 
         public void setRotation(float addDegree)
         {
             center.transform.rotation = Quaternion.AngleAxis(center.transform.rotation.eulerAngles.z + addDegree, Vector3.forward);
         }
 
-        public Mesh UnifyMeshes(Mesh mesh1, Mesh mesh2)
-        {
-            // Create a new mesh
-            Mesh unifiedMesh = new Mesh();
 
-            // Combine vertices
-            List<Vector3> vertices = new List<Vector3>(mesh1.vertices);
-            vertices.AddRange(mesh2.vertices);
+        public MeshInfo UnifyMeshes(MeshInfo mesh1, MeshInfo mesh2)
+        {
+            // Combine vertices (using AddRange makes the proces more eficient)
+            List<Vector3> vertices = new List<Vector3>(mesh1.getVertices());
+            int vertexOffset = vertices.Count;
+            vertices.AddRange(mesh2.getVertices());
 
             // Combine triangles
-            List<int> triangles = new List<int>(mesh1.triangles);
+            List<int> trianglesM1 = new List<int>(mesh1.getTriangles());
+            List<int> trianglesM2 = new List<int>(mesh2.getTriangles());
+
             // Adjust triangle indices for mesh2
-            int vertexOffset = mesh1.vertices.Length;
-            for (int i = 0; i < mesh2.triangles.Length; i++)
+            for (int i = 0; i < trianglesM2.Count; i++)
             {
-                triangles.Add(mesh2.triangles[i] + vertexOffset);
+                trianglesM1.Add(trianglesM2[i] + vertexOffset);
             }
 
             // Assign combined vertices and triangles to the new mesh
-            unifiedMesh.vertices = vertices.ToArray();
-            unifiedMesh.triangles = triangles.ToArray();
+            MeshInfo unifiedMesh = new MeshInfo(vertices.ToArray(),trianglesM1.ToArray());            
 
             // Recalculate normals
-            unifiedMesh.RecalculateNormals();
+            //unifiedMesh.RecalculateNormals();-------------------------------------------------------------------------------------------------------------------------------------------------
 
             return unifiedMesh;
         }
 
-        public Mesh addMeshToPosition(Mesh mesh, Vector3 bottomPosMesh, float angleMesh, Vector3 finalPos, float finalAngle)
+        public MeshInfo UnifyTwoMeshes(MeshInfo mesh1, MeshInfo mesh2)
         {
-            //checkpoit del spawner
-            Vector3 spanwerPos = center.transform.localPosition;
-            float spawnerAngle = center.transform.localEulerAngles.z;
+            List<int> trianglesM1 = new List<int>(mesh1.getTriangles());
+            List<int> trianglesM2 = new List<int>(mesh2.getTriangles());
 
-            center.transform.localPosition = bottomPosMesh;
-            center.transform.rotation = Quaternion.AngleAxis(angleMesh, Vector3.forward);
+            List<Vector3> vertices = new List<Vector3>(mesh1.getVertices());
+            int vertexOffset = vertices.Count();
 
-            meshFilterRecipient.mesh = mesh;
+            List<Vector3> verticesM2 = new List<Vector3>(mesh2.getVertices());
+            bool doMerge = verticesM2.Count() >= 2;
 
-            center.transform.localPosition = finalPos;
-            center.transform.rotation = Quaternion.AngleAxis(finalAngle, Vector3.forward);
+            if (doMerge)
+            {
+                verticesM2.RemoveAt(0);
+                verticesM2.RemoveAt(0);
 
-            //recetegem el spawner on era i com estave
-            //meshFilterRecipient.mesh = new Mesh();
-            //addPositionAndDegree(spanwerPos, spawnerAngle);
-            return null;
+                trianglesM2[0] = vertices.Count() - 2; 
+                trianglesM1.Add(trianglesM2[0]);
+
+                trianglesM2[1] = trianglesM2[1] + vertexOffset - 2;
+                trianglesM1.Add(trianglesM2[1]);
+
+                trianglesM2[2] = vertices.Count() - 1; 
+                trianglesM1.Add(trianglesM2[2]);
+            }
+
+            vertices.AddRange(verticesM2);
+
+            for (int i = doMerge? 3 : 0; i < trianglesM2.Count; i++)
+            {
+                if (doMerge)
+                {
+                    trianglesM1.Add(trianglesM2[i] + vertexOffset - 2);
+                }
+                else trianglesM1.Add(trianglesM2[i] + vertexOffset);
+            }
+
+            MeshInfo unifiedMesh = new MeshInfo(vertices.ToArray(), trianglesM1.ToArray());
+
+            return unifiedMesh;
         }
 
-        public Mesh grow(int lenghtY, Mesh mesh)
+        public MeshInfo grow(int lenghtY, MeshInfo meshInfo)
         {
             int yExtra = 0;
+            List<Vector3> vertices = new List<Vector3>(meshInfo.getVertices());
+            List<int> triangles = new List<int>(meshInfo.getTriangles());
+
             //estem generant de 0 o ja tenim mesh generada?
-            if (mesh.vertexCount != 0)
+            if (!meshInfo.isEmpty())
             {
                 growCenter.transform.parent = parent.transform;
 
@@ -234,11 +304,14 @@ public class VegetableProxyMesh : MonoBehaviour
                 right.transform.parent = parent.transform;
 
                 newVertices[i++] = left.transform.localPosition;
+
                 newVertices[i++] = right.transform.localPosition;
 
 
                 left.transform.parent = center.transform;
                 right.transform.parent = center.transform;
+
+
                 if (y + 1 <= (lenghtY + yExtra))
                 {
                     growCenter.transform.parent = parent.transform;
@@ -250,8 +323,7 @@ public class VegetableProxyMesh : MonoBehaviour
                 }
             }
 
-
-            int vIndex = (mesh.triangles.Length / 6) * 2;
+            int vIndex = (triangles.Count / 6) * 2;
             int[] newTriangles = new int[lenghtY * 6];
 
             for (int y = 0, mesT = 0; y < lenghtY; y++)
@@ -268,21 +340,22 @@ public class VegetableProxyMesh : MonoBehaviour
                 vIndex += 2;
             }
 
-            mesh.vertices = mesh.vertices.Concat(newVertices).ToArray();
-            mesh.triangles = mesh.triangles.Concat(newTriangles).ToArray();
+            vertices.AddRange(newVertices);
+            triangles.AddRange(newTriangles);
 
-            mesh.RecalculateNormals();
-            return mesh;
+            meshInfo.setVertices(vertices.ToArray());
+            meshInfo.setTriangles(triangles.ToArray());
 
+            return meshInfo;
         }
 
-        public bool translateStandartRules(char value, Mesh mesh)
+        public bool translateStandartRules(char value, MeshInfo meshInfo)
         {
             switch ((Rules.DNAnucleotides)value)
             {
                 case Rules.DNAnucleotides.GROW:
                     //creix la mesh acutal
-                    grow(1, mesh);
+                    grow(1, meshInfo);
                     break;
                 case Rules.DNAnucleotides.POSITIVE_ROTATION:
                     //rotem en +
@@ -335,6 +408,7 @@ public class VegetableProxyMesh : MonoBehaviour
         GOspwaner.setPositionAndDegree(Vector3.zero, transform.localEulerAngles.z);
 
         activeDNA = ((char)Rules.DNAnucleotides.NONE).ToString();
+        vegetableMesh = new Mesh();
     }
 
 
@@ -355,9 +429,16 @@ public class VegetableProxyMesh : MonoBehaviour
     {
         if (myVegetable == null) throw new System.NotImplementedException();
         activeDNA = myVegetable.pasTime(activeDNA);
+
         meshNode = new MeshNode(GOspwaner.getPositionCenter(), GOspwaner, 0);//iniciem la generacio de 0 sempre
-        GetComponent<MeshFilter>().mesh = meshNode.startGeneration(activeDNA).mesh;
-        GOspwaner.setPositionAndDegree(Vector3.zero, transform.localEulerAngles.z);//recetegem la posicio del spawner
+        MeshInfo meshInfo = meshNode.startGeneration(activeDNA).getMeshInfo();
+
+        vegetableMesh.Clear();
+        vegetableMesh.vertices = meshInfo.getVertices();
+        vegetableMesh.triangles = meshInfo.getTriangles();
+        vegetableMesh.RecalculateNormals();
+        GetComponent<MeshFilter>().mesh = vegetableMesh;
+        GOspwaner.resetAllPositions();//recetegem la posicio del spawner
         return activeDNA;
     }
 
